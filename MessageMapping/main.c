11c47f46a782565a9503e49b287b02c0d7fe2f77
CFWLoader <cfwloader@gmail.com>
2016-11-12 20:15:56 +0800

    Finished a simple ConfResolver.

--- MessageMapping/main.c
+++ MessageMapping/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+
+#include "aux/ConfResolver.h"
 
 int main(int argc, char** argv)
 {
-	printf("Testing.");
-	
+	struct FunProp* funProp = NULL;
+
+	int result = readConfFile("conf/sample.conf", &funProp);
+
+	struct FunProp* ptr = funProp;
+
+	while(ptr != NULL)
+	{
+		printf("Key: %s, FunName: %s, LibPath: %s, FunEntry: %s.\n",
+			ptr->key,
+			ptr->funName,
+			ptr->libPath,
+			ptr->funEntry);
+
+		ptr = ptr->next;
+	}
+
+	destroyFunProp(&funProp);
+
+	printf("Read Code: %d.\n", result);
+
+	//printf("Test strcmp: %d.\n", strcmp("aaav", "aaa"));
+
 	return 0;
 }
