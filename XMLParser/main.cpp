e90ab1d2844dc82d93dbc19b0f155f57abba62de
CFWLoader <cfwloader@gmail.com>
2017-03-30 15:51:44 +0800

    Review this little project.

---
+++ XMLParser/main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by CFWLoader on 3/30/17.
+//
+
+#include <iostream>
+
+#include "parser/XMLParser.h"
+
+using XMLUtilities::XMLParser;
+using XMLUtilities::XMLObject;
+
+int main(int argc, char* argv[])
+{
+    XMLParser parser;
+
+    XMLObject object;
+
+    return 0;
+}
