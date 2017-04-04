6bafbc361f14b840701014617603a04b2e796afa
CFWLoader <cfwloader@gmail.com>
2017-04-04 19:28:59 +0800

    Enlarge the data interval to see the advantage of ideas.

--- ForHomework/DMCT-ch3/Q3-13c-v2.py
+++ ForHomework/DMCT-ch3/Q3-13c-v2.py
@@ -10,9 +10,9 @@ for count in range(0, 1000, 1):
 
 salary_statistic.sort()
 
-statistic_interval = 100
+statistic_interval = 500
 
-upper_bound = 2000
+upper_bound = 3500
 
 statistic_buckets = []
 
@@ -42,11 +42,30 @@ while statistic_interval <= upper_bound:
 
     statistic_buckets.append(current_bucket)
 
-    statistic_interval += 100
+    statistic_interval += 500
 
+std_collector = []
 
 for ele in statistic_buckets:
 
-    print(ele)
+    std_collector.append(numpy.std(ele))
 
-    print(numpy.std(ele))
+print(std_collector)
+
+# sc_mean = numpy.mean(std_collector)
+#
+# idx = 0
+#
+# marked_idx = 0
+#
+# dt = numpy.abs(sc_mean - std_collector[0])
+#
+# for element in std_collector:
+#
+#     if numpy.abs(sc_mean - element) < dt:
+#
+#         marked_idx = idx
+#
+#     idx += 1
+#
+# print(marked_idx, '-', std_collector[marked_idx], '-', sc_mean - std_collector[marked_idx])
