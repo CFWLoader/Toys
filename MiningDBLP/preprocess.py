35ad15527fa0d95ab96bdd24a952c1bd42bf0c5d
CFWLoader <cfwloader@gmail.com>
2017-04-05 13:31:46 +0800

    Start coding for DMCT homework.

---
+++ MiningDBLP/preprocess.py
@@ -0,0 +1,51 @@
+import codecs
+from xml.sax import handler, make_parser
+
+paper_tag = ('article', 'inproceedings', 'proceedings', 'book', 'incollection', 'phdthesis', 'mastersthesis', 'www')
+
+class LoadHandler(handler.ContentHandler):
+
+    def __init__(self, result):
+
+        super().__init__()
+
+        self.result = result
+
+        self.flag = 0
+
+    def startDocument(self):
+
+        print('Document Start.')
+
+    def endDocument(self):
+
+        print('Document End.')
+
+    def startElement(self, name, attrs):
+
+        if name == 'author':
+
+            self.flag = 1
+
+    def endElement(self, name):
+
+        if name == 'author':
+
+            self.result.write('\r\n')
+
+            self.flag = 0
+
+        if name in paper_tag:
+
+            self.result.write('\r\n')
+
+
+    def characters(self, content):
+
+        if self.flag:
+
+            self.result.write(content)
+
+def parseDblpXml(source, result):
+
+    temp_handler = LoadHandler()
