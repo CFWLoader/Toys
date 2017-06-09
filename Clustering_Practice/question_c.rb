288c5dd0758f08a43f69460131dfb6747d56b659
CFWLoader <cfwloader@gmail.com>
2017-06-09 17:03:23 +0800

    Start solving question c.

--- Clustering_Practice/question_c.rb
+++ Clustering_Practice/question_c.rb
@@ -1,1 +1,23 @@
 # points = [6, 12, 18, 24, 30, 42, 48]
+
+def min_distance_between_clusters cluster1, cluster2
+
+  min_dist = (cluster1[0] - cluster2[0]).abs
+
+  cluster1.each{|p1|
+
+    cluster2.each{|p2|
+
+      if min_dist > (p1 - p2).abs
+
+        min_dist = (p1 - p2).abs
+
+      end
+
+    }
+
+  }
+
+  min_dist
+
+end
