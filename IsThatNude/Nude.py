5f4baaeeb8fb4688874b8e02d5ebb4d65420ea60
Evan Huang <cfwloader@gmail.com>
2017-09-12 16:30:36 +0800

    Checkpoint.

--- IsThatNude/Nude.py
+++ IsThatNude/Nude.py
@@ -206,3 +206,89 @@ class Nude(object):
 
     def _classify_skin(self, r, g, b):
 
+        # Predict by RGB values.
+        rgb_classifier = r > 95 and \
+            g > 40 and g < 100 and \
+            b > 20 and \
+            max([r, g, b]) - min([r, g, b]) > 15 and \
+            abs(r - g) > 15 and \
+            r > g and \
+            r > b
+
+        # Predict by processed RGB values.
+        nr, ng, nb = self._to_normalized(r, g, b)
+
+        norm_rgb_classifier = nr / ng > 1.185 and \
+            float(r * g) / ((r + g + b) ** 2) > 0.107 and \
+            float(r * g) / ((r + g + b) ** 2) > 0.112
+
+        # Predict by HSV color mode.
+        h, s, v = self._to_hsv(r, g, b)
+
+        hsv_classifier = h > 0 and \
+            h < 35 and \
+            s > 0.23 and \
+            s < 0.68
+
+        # Predict by YCbCr color mode.
+        y, cb, cr = self._to_ycbcr(r, g, b)
+
+        ycbcr_classifier = 97.5 <= cb <= 142.5 and 134 <= cr <= 176
+
+        return ycbcr_classifier
+
+    def _to_normalized(self, r, g, b):
+        if r == 0:
+            r = 0.0001
+        if g == 0:
+            g = 0.0001
+        if b == 0:
+            b = 0.0001
+        _sum = float(r + g + b)
+        return [r / _sum, g / _sum, b / _sum]
+
+    def _to_ycbcr(self, r, g, b):
+        # 公式来源：
+        # http://stackoverflow.com/questions/19459831/rgb-to-ycbcr-conversion-problems
+        y = .299 * r + .587 * g + .114 * b
+        cb = 128 - 0.168736 * r - 0.331364 * g + 0.5 * b
+        cr = 128 + 0.5 * r - 0.418688 * g - 0.081312 * b
+        return y, cb, cr
+
+    def _to_hsv(self, r, g, b):
+        h = 0
+        _sum = float(r + g + b)
+        _max = float(max([r, g, b]))
+        _min = float(min([r, g, b]))
+        diff = float(_max - _min)
+        if _sum == 0:
+            _sum = 0.0001
+
+        if _max == r:
+            if diff == 0:
+                h = sys.maxsize
+            else:
+                h = (g - b) / diff
+        elif _max == g:
+            h = 2 + ((g - r) / diff)
+        else:
+            h = 4 + ((r - g) / diff)
+
+        h *= 60
+        if h < 0:
+            h += 360
+
+        return [h, 1.0 - (3.0 * (_min / _sum)), (1.0 / 3.0) * _max]
+
+    def _add_merge(self, _from, _to):
+
+        self.last_from = _from
+
+        self.last_to = _to
+
+        from_index = -1
+
+        to_index = -1
+
+        for index, region in enumerate(self.merge_regions):
+
