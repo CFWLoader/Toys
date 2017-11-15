ce5a655538765e3b0c339683a656088a694519ba
Evan Huang <cfwloader@gmail.com>
2017-11-15 11:05:19 +0800

    Visualized some data.

--- DMtoys/Weather/mining_weather.py
+++ DMtoys/Weather/mining_weather.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 
 '''
 0 - date-time
@@ -86,22 +87,177 @@ def outliers(data_tuples, item_index, threshold, ave = None):
     return outliers_
 
 
+def plot_item(data_tuples, item_index, ave = None, var = None):
+
+    x = range(len(data_tuples))
+
+    y = []
+
+    y_ave = [ave] * len(data_tuples) if ave is not None else []
+
+    y_var_up = [ave + var] * len(data_tuples) if var is not None else []
+
+    y_var_low = [ave - var] * len(data_tuples) if var is not None else []
+
+    for idx, tup in enumerate(data_tuples):
+
+        # x.append(idx)
+
+        y.append(tup[item_index])
+
+    # plt.scatter(x, y, label= 'Showing')
+
+    plt.plot(x, y, label='Val.')
+
+    if ave is not None:
+
+        plt.plot(x, y_ave, label='Ave.')
+
+    if var is not None:
+
+        plt.plot(x, y_var_up, label='Var+')
+
+        plt.plot(x, y_var_low, label='Var-')
+
+    plt.xlabel('Timestamp')
+
+    plt.ylabel('Value')
+
+    plt.title('Every Day Weather')
+
+    plt.legend()
+
+    plt.show()
+
+
+def plot_weather_statics1(data_tuples):
+
+    data_len = len(data_tuples)
+
+    x = range(data_len)
+
+    ap = [(lambda x:x[1])(var) for var in data_tuples]
+
+    ave = average(data_tuples, 1)
+
+    var = variance(data_tuples, 1, ave)
+
+    ap_ave = [ave] * data_len
+
+    ap_var_u = [ave + var] * data_len
+
+    ap_var_l = [ave - var] * data_len
+
+    rain = [(lambda x:x[2])(var) for var in data_tuples]
+
+    ave = average(data_tuples, 2)
+
+    var = variance(data_tuples, 2, ave)
+
+    rain_ave = [ave] * data_len
+
+    rain_var_u = [ave + var] * data_len
+
+    rain_var_l = [ave - var] * data_len
+
+    ws =  [(lambda x:x[3])(var) for var in data_tuples]
+
+    ave = average(data_tuples, 3)
+
+    var = variance(data_tuples, 3, ave)
+
+    ws_ave = [ave] * data_len
+
+    ws_var_u = [ave + var] * data_len
+
+    ws_var_l = [ave - var] * data_len
+
+    wd = [(lambda x:x[4])(var) for var in data_tuples]
+
+    ave = average(data_tuples, 4)
+
+    # var = variance(data_tuples, 4, ave)
+
+    wd_ave = [ave] * data_len
+
+    # wd_var_u = [ave + var] * data_len
+    #
+    # wd_var_l = [ave - var] * data_len
+
+    # st = []
+    #
+    # rh = []
+    #
+    # sf = []
+    #
+    # bat = []
+
+    plt.figure()
+
+    p1 = plt.subplot(221)
+
+    p2 = plt.subplot(222)
+
+    p3 = plt.subplot(223)
+
+    p4 = plt.subplot(224)
+
+    p1.plot(x, ap)
+    p1.plot(x, ap_ave)
+    p1.plot(x, ap_var_u)
+    p1.plot(x, ap_var_l)
+    p1.set_title('Atmospheric Pressure (mBar)')
+
+    p2.plot(x, rain)
+    p2.plot(x, rain_ave)
+    p2.plot(x, rain_var_u)
+    p2.plot(x, rain_var_l)
+    p2.set_title('Rainfall (mm)')
+
+    p3.plot(x, ws)
+    p3.plot(x, ws_ave)
+    p3.plot(x, ws_var_u)
+    p3.plot(x, ws_var_l)
+    p3.set_title('Wind Speed (m/s)')
+
+    p4.plot(x, wd)
+    p4.plot(x, wd_ave)
+    # p4.plot(x, wd_var_u)
+    # p4.plot(x, wd_var_l)
+    p4.set_title('Wind Direction (degrees)')
+
+    p1.legend()
+    p2.legend()
+    p3.legend()
+    p4.legend()
+
+    plt.show()
+
+
 if __name__ == '__main__':
 
     data = load_data('data/JCMB_2015.csv')
 
-    data_len = len(data)
+    plot_weather_statics1(data)
 
-    data_item_index = 8
+    # ap = [(lambda x:x[1])(tup) for tup in data]
 
-    ave = average(data, data_item_index)
+    # print(ap[1])
 
-    var = variance(data, data_item_index, ave)
+    # data_len = len(data)
+    #
+    # data_item_index = 5
+    #
+    # plot_item(data, data_item_index, average(data, data_item_index), variance(data, data_item_index))
 
-    for multiple_val in range(1, 10):
-
-        print('Threshold = ', var * multiple_val, ' Multiple = ', multiple_val, end='')
-
-        outliers_arr = outliers(data, data_item_index, var * multiple_val, ave)
-
-        print(', Outliers = ', len(outliers_arr), ' Percentage = ', len(outliers_arr) / data_len * 100, '%')
+    # ave = average(data, data_item_index)
+    #
+    # var = variance(data, data_item_index, ave)
+    #
+    # for multiple_val in range(1, 10):
+    #
+    #     print('Threshold = ', var * multiple_val, ' Multiple = ', multiple_val, end='')
+    #
+    #     outliers_arr = outliers(data, data_item_index, var * multiple_val, ave)
+    #
+    #     print(', Outliers = ', len(outliers_arr), ' Percentage = ', len(outliers_arr) / data_len * 100, '%')
