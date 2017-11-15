580fe671eb93b7cf25da24ef2ed3f36ee920b0c3
Evan Huang <cfwloader@gmail.com>
2017-11-15 11:54:49 +0800

    Launch time.

---
+++ DMtoys/TateGallery/mining_tate_gallery.py
@@ -0,0 +1,49 @@
+import csv
+
+
+def load_data(file_path, fields = None):
+
+    file = open(file_path, 'r')
+
+    # fileds = ['id',
+    #           'accession_number',
+    #           'artist',
+    #           'artistRole',
+    #           'artistId',
+    #           'title',
+    #           'dateText',
+    #           'medium',
+    #           'creditLine',
+    #           'year',
+    #           'acquisitionYear',
+    #           'dimensions',
+    #           'width',
+    #           'height',
+    #           'depth',
+    #           'units',
+    #           'inscription',
+    #           'thumbnailCopyright',
+    #           'thumbnailUrl',
+    #           'url']
+
+    reader = csv.DictReader(file, fields)
+
+    static_lst = []
+
+    for obj in reader:
+        static_lst.append(obj)
+
+    file.close()
+
+    return static_lst
+
+
+# def number_of_artist
+
+
+if __name__ == '__main__':
+
+    data_list = load_data('data/artwork_data.csv')
+
+    print(data_list[0]['\ufeffid'])
+    print(data_list[1]['\ufeffid'])
