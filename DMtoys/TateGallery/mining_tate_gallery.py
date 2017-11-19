0d4019a1d1a6c2f67f770ccd4dc5124301c711a1
Evan Huang <cfwloader@gmail.com>
2017-11-19 23:01:37 +0800

    Answered some questions.

--- DMtoys/TateGallery/mining_tate_gallery.py
+++ DMtoys/TateGallery/mining_tate_gallery.py
@@ -38,13 +38,57 @@ def load_data(file_path, fields = None):
     return static_lst
 
 
-# def number_of_artist
+def number_of_artist_and_artwork(data_list):
+
+    artists_count = {}
+
+    artwork_count = {}
+
+    for record in data_list:
+
+        if record['artist'] in artists_count:
+
+            artists_count[record['artist']] += 1
+
+        else:
+
+            artists_count[record['artist']] = 1
+
+        if record['id'] in artwork_count:
+
+            artwork_count[record['id']] += 1
+
+        else:
+
+            artwork_count[record['id']] = 1
+
+    if len(artwork_count) != len(data_list):
+
+        raise Exception('Statistic Mismatch!')
+
+    return artists_count, artwork_count
 
 
 if __name__ == '__main__':
 
     data_list = load_data('data/artwork_data.csv')
 
-    print(data_list[0]['id'])
-    print(data_list[1]['id'])
-    print('Just for a commit.')
+    artist_counter, artwork_counter = number_of_artist_and_artwork(data_list)
+
+    print("Number of Artist: %d, Number of Artwork: %d." % (len(artist_counter), len(artwork_counter)))
+
+    result = sorted(artist_counter.items(), key= lambda a_pair: a_pair[1], reverse= True)
+
+    top_count = 0
+
+    for (name, count) in result:
+
+        print("Artist: %s, Number of his/her works: %d." % (name, count))
+
+        top_count += 1
+
+        if top_count == 10:
+
+            break
+
+    print("Warhol, Andy has %d works in Tate." % artist_counter['Warhol, Andy'])
