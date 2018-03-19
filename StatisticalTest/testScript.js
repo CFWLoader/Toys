4c7f0dd02e3eedebba9f63b5e1dc6dfd931c639e
CFWLoader <cfwloader@gmail.com>
2018-03-19 17:52:22 +0800

    Save before off-duty.

--- StatisticalTest/testScript.js
+++ StatisticalTest/testScript.js
@@ -37,13 +37,37 @@ function batchTest(dist_data) {
     console.log("|Weibull|" + formatDisplay(adTest.pValueWeibull(dist_data)) + "|" + formatDisplay(ksTest.pValueWeibull(dist_data)) + "|");
 
 }
+function batchTestReport(dist_data) {
+
+    var adValue, adPvalue, ksValue, ksPvalue;
+
+    console.log("Beta: N/A N/A");
+
+    // adValue = adTest.test(dist_data)
+    
+    console.log("|Exponential|" + formatDisplay(adTest.pValueExponential(dist_data)) + "|" + formatDisplay(ksTest.pValueExponential(dist_data)) + "|");
+    
+
+    console.log("|Normal|" + formatDisplay(adTest.pValueNormal(dist_data)) + "|" + formatDisplay(ksTest.pValueNormal(dist_data)) + "|");    
+    
+    console.log("|Log-Normal|" + formatDisplay(adTest.pValueLogNormal(dist_data)) + "|" + formatDisplay(ksTest.pValueLogNormal(dist_data)) + "|");
+
+    console.log("|Uniform|" + formatDisplay(adTest.pValueUniform(dist_data)) + "|" + formatDisplay(ksTest.pValueUniform(dist_data)) + "|");
+
+    console.log("|Triangular|N/A|N/A|");
+
+    console.log("|Gamma|" + formatDisplay(adTest.pValueGamma(dist_data)) + "|" + formatDisplay(ksTest.pValueGamma(dist_data)) + "|");
+
+    console.log("|Weibull|" + formatDisplay(adTest.pValueWeibull(dist_data)) + "|" + formatDisplay(ksTest.pValueWeibull(dist_data)) + "|");
+
+}
 
 var fs = require('fs');
 
-// console.log("AQI:")
-// var file = ".\\aqi_dist.json";
-// var dist_data = JSON.parse(fs.readFileSync(file));
-// batchTest(dist_data);
+console.log("AQI:")
+var file = ".\\aqi_dist.json";
+var dist_data = JSON.parse(fs.readFileSync(file));
+batchTest(dist_data);
 
 // console.log("PM2.5:")
 // var file = ".\\pm25_dist.json"
@@ -80,7 +104,7 @@ var fs = require('fs');
 // var dist_data = JSON.parse(fs.readFileSync(file));
 // batchTest(dist_data);
 
-console.log("Weibull:");
-var file = ".\\weib_dist1.json";
-var dist_data = JSON.parse(fs.readFileSync(file));
-batchTest(dist_data);
+// console.log("Weibull:");
+// var file = ".\\weib_dist1.json";
+// var dist_data = JSON.parse(fs.readFileSync(file));
+// batchTest(dist_data);
