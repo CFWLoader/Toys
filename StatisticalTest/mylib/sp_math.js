8493410f5b8631da471c7d336c0cdf40aeb8a4ec
CFWLoader <cfwloader@gmail.com>
2018-03-27 15:42:25 +0800

    Grammar upgrade of spMath and test.

--- StatisticalTest/mylib/sp_math.js
+++ StatisticalTest/mylib/sp_math.js
@@ -5,11 +5,11 @@ import mathjs from 'mathjs';
 import digamma from 'math-digamma';
 
 /**
- * Calculate gamma(x).
+ * Calculate complete gamma(x).
  * @param {Number} x 
  * @returns {Number}
  */
-function gamma(x)
+function cgamma(x)
 {
     const p = [0.99999999999980993, 676.5203681218851, -1259.1392167224028,
         771.32342877765313, -176.61502916214059, 12.507343278686905,
@@ -32,7 +32,7 @@ function gamma(x)
         a += p[i] / (x + i);
     }
  
-    return mathjs.sqrt(2 * matjs.PI) * mathjs.pow(t, x + 0.5) * mathjs.exp(-t) * a;
+    return mathjs.sqrt(2 * mathjs.PI) * mathjs.pow(t, x + 0.5) * mathjs.exp(-t) * a;
 }
 
 /**
@@ -144,8 +144,8 @@ function variance(data)
 
 /**
  * Optimized Newtow's method for approximate coefficients of original function.
- * @param {Array} firstDrv First derivate of original function. AKA Jacobian Matrix.
- * @param {Array[]} secondDrv Second derivate of original function. AKA Hessian Matrix.
+ * @param {Array} firstDrv First derivative of original function. AKA Jacobian Matrix.
+ * @param {Array[]} secondDrv Second derivative of original function. AKA Hessian Matrix.
  * @param {Array} xArray Observed values.
  * @param {Array} initialVal 
  * @param {Number} epsilon
@@ -164,29 +164,12 @@ function newtonMethodOpt2Var(firstDrv, secondDrv, xArray, initialVal, epsilon = 
 
 	let jaccob = [firstDrv[0](x[0], x[1], xArray), firstDrv[1](x[0], x[1], xArray)];
 
-	// console.log(firstDrv[0](x[0], x[1]));
-
 	let deltax = mathjs.multiply(hessianInv, jaccob);
 
 	let xn = [x[0] - deltax[0], x[1] - deltax[1]];
 
-	// console.log("Delta: " + deltax.toString());
-
-	// return {"alpha" : 0, "beta" : 0};
-
-	// var iterations = 0;
-
-	// while(iterations < 2000)
 	while(mathjs.abs(xn[0] - x[0]) > epsilon || mathjs.abs(xn[1] - x[1]) > epsilon)
 	{
-		// ++iterations;
-
-		// console.log("[" + iterations.toString() + "]:" + mathjs.abs(xn[0] - x[0]).toString() + "   " + mathjs.abs(xn[1] - x[1]).toString());
-
-		// console.log("Iteration: " + iterations.toString());
-
-		// console.log("X:" + x.toString());
-
 		x[0] = xn[0];
 
 		x[1] = xn[1];
@@ -203,222 +186,166 @@ function newtonMethodOpt2Var(firstDrv, secondDrv, xArray, initialVal, epsilon = 
 		deltax = mathjs.multiply(hessianInv, jaccob);
 
 		xn = [x[0] - deltax[0], x[1] - deltax[1]];
-	
-		// console.log(hessianInv[0]);
-
-		// console.log(hessianInv[1]);
-
-		// console.log(jaccob);
-
-		// console.log("Delta: " + deltax.toString());
 	}
-
-	// console.log("Newton iterated " + iterations.toString());
 
 	return {"shape1" : x[0] - deltax[0], "shape2" : x[1] - deltax[1]};
-
-	// while(mathjs.abs(xn[0] - x[0]) > epsilon && mathjs.abs(xn[1] - x[0]))
-	// {
-	// 	x[0] = xn[0], x[1] = xn[1];
-	// }
 }
 
-// Using maximum likehood. https://en.wikipedia.org/wiki/Gamma_distribution#Maximum_likelihood_estimation
+/**
+ * Retrieve parameters of gamma distribution.
+ * Using maximum likehood. https://en.wikipedia.org/wiki/Gamma_distribution#Maximum_likelihood_estimation
+ * @param {Array} data
+ * @returns {Key-Value}
+ */
 function gammaParameters(data)
 {
-	var mu = mean(data), sigma = mathjs.sqrt(variance(data));
-
-    var logMu = 0, len = data.length, logSumVal = 0, sumVal = 0;
-
-    for(var i = 0; i < data.length; ++i)
-    {
-        if(data[i] <= 0)
-        {
-            return [];
-        }
-
-		logMu += mathjs.log(data[i]);
-		
-		sumVal += data[i];
-	}
-	
-	logSumVal = logMu;
-
-	logMu /= data.length;
-
-	// console.log("gp: len=" + len + ", log sum=" + logSumVal.toString() + ", sum=" + sumVal);
-	
-	var f1 = function(a, b, xArr)
+	if(data.length == 0)
 	{
-		// console.log("Len(f1): " + len.toString() + "   Log sum: " + logSumVal.toString());
-
-		return len * mathjs.log(b) - len * digamma(a) + logSumVal;
+		return {};
 	}
 
-	var f2 = function(a, b, xArr)
+	let mu = mean(data), sigma = mathjs.sqrt(variance(data));
+
+	let f1 = (a, b, xArr) =>
 	{
-		// console.log("Len(f2): " + len.toString() + "   Sum: " + sumVal.toString());
+		let logSumVal = xArr.reduce((pre, val) => pre + mathjs.log(val), 0);
 
-		return len * a / b - sumVal;
+		return xArr.length * mathjs.log(b) - xArr.length * digamma(a) + logSumVal;
 	}
 
-	var f11 = function(a, b, xArr)
+	let f2 = (a, b, xArr) =>
 	{
-		return -len * trigamma(a);
+		let sumVal = xArr.reduce((pre, val) => pre + val, 0);
+
+		return xArr.length * a / b - sumVal;
 	}
 
-	var f12 = function(a, b, xArr)
+	let f11 = (a, b, xArr) =>
 	{
-		return len / b;
+		return -xArr.length * trigamma(a);
 	}
 
-	var f21 = f12;
-
-	var f22 = function(a, b, xArr)
+	let f12 = (a, b, xArr) =>
 	{
-		return - len * a / b**2;
+		return xArr.length / b;
 	}
 
-	var alpha = (mu / sigma)**2, beta = mu / sigma**2;
+	let f21 = f12;
 
-	// var s = mathjs.log(mu) - logMu;
+	let f22 = (a, b, xArr) =>
+	{
+		return - xArr.length * a / b**2;
+	}
 
-	// var alpha = (3 - s + mathjs.sqrt((s - 3)**2 + 24 * s)) / (12 * s);
-
-	// var beta = alpha / mu;
+	let alpha = (mu / sigma)**2, beta = mu / sigma**2;
 
 	return newtonMethodOpt2Var([f1, f2], [[f11, f12], [f21, f22]], data, [alpha, beta]);
-
-    // var s = mathjs.log(mu) - logMu;
-
-	// var alpha = (3 - s + mathjs.sqrt((s - 3)**2 + 24 * s)) / (12 * s);
-	
-	// var nextAlpha = alpha - (mathjs.log(alpha) - digamma(alpha) - s) / (1 / alpha - spMath.trigamma(alpha));
-
-	// while(nextAlpha - alpha > 1e-12)
-	// {
-	// 	alpha = nextAlpha;
-
-	// 	nextAlpha = alpha - (mathjs.log(alpha) - digamma(alpha) - s) / (1 / alpha - spMath.trigamma(alpha));
-	// }
 }
 
+/**
+ * Retrieve parameters of beta distribution.
+ * @param {Array} data 
+ * @returns {Key-Value}
+ */
 function betaParameters(data)
 {
-	var obsX = mean(data);
-
-	var obsVar = variance(data);
-
-	var f1 = function(a, b, xArr)
+	if(data.length == 0)
 	{
-		var lnSum = 0, len = xArr.length;
+		return {};
+	}
 
-		for(var i = 0; i < len; ++i)
+	for(let val of data)
+	{
+		if(val <= 0 || val >= 1)
 		{
-			lnSum += mathjs.log(xArr[i]);
+			return {};
 		}
+	}
+
+	let obsX = mean(data);
+
+	let obsVar = variance(data);
+
+	let f1 = (a, b, xArr) =>
+	{
+		let lnSum = xArr.reduce((pre, val) => pre + mathjs.log(val), 0), 
+			len = xArr.length;
 
 		return len * digamma(a + b) - len * digamma(a) + lnSum;
 	}
 
-	var f2 = function(a, b, xArr)
+	let f2 = (a, b, xArr) =>
 	{
-		var lnSum = 0, len = xArr.length;
-
-		for(var i = 0; i < len; ++i)
-		{
-			lnSum += mathjs.log(1 - xArr[i]);
-		}
+		let lnSum = xArr.reduce((pre, val) => pre + mathjs.log(1 - val), 0),
+			len = xArr.length;
 
 		return len * digamma(a + b) - len * digamma(b) + lnSum;
 	}
 
-	var f11 = function(a, b, xArr)
+	let f11 = (a, b, xArr) =>
 	{
 		return xArr.length * (trigamma(a + b) - trigamma(a));
 	}
 
-	var f12 = function(a, b, xArr)
+	let f12 = (a, b, xArr) =>
 	{
 		return xArr.length * trigamma(a + b);
 	}
 
-	var f21 = f12;
+	let f21 = f12;
 
-	var f22 = function(a, b, xArr)
+	let f22 = (a, b, xArr) =>
 	{
 		return xArr.length * (trigamma(a + b) - trigamma(b));
 	}
 
-	var alpha = obsX * (obsX * (1 - obsX) / obsVar - 1), beta = (1 - obsX) * (obsX * (1 - obsX) / obsVar - 1);
+	let alpha = obsX * (obsX * (1 - obsX) / obsVar - 1), beta = (1 - obsX) * (obsX * (1 - obsX) / obsVar - 1);
 
 	return newtonMethodOpt2Var([f1, f2], [[f11, f12], [f21, f22]], data, [alpha, beta]);
 }
 
+/**
+ * Retrieve parameters of Weibull distribution.
+ * @param {Array} data 
+ * @returns {Key-Value}
+ */
 function weibullParameters(data)
 {
-	var f1 = function(a, b, xArr)
+	let f1 = (a, b, xArr) =>
 	{
-		var arraySum = 0;
-
-		for(var i = 0; i < xArr.length; ++i)
-		{
-			arraySum += mathjs.pow(xArr[i] / a, b);
-		}
+		let arraySum = xArr.reduce((pre, val) => pre + mathjs.pow(val / a, b), 0);
 
 		return - xArr.length * b / a + b * arraySum / a;
 	}
 
-	var f2 = function(a, b, xArr)
+	let f2 = (a, b, xArr) =>
 	{
-		var lnSum = 0, arraySum = 0;
-
-		for(var i = 0; i < xArr.length; ++i)
-		{
-			lnSum += mathjs.log(xArr[i]);
-
-			arraySum += (mathjs.pow(xArr[i] / a, b) * mathjs.log(xArr[i] / a));
-		}
+		let lnSum = xArr.reduce((pre, val) => pre + mathjs.log(val), 0), 
+			arraySum = xArr.reduce((pre, val) => pre + mathjs.pow(val / a, b) * mathjs.log(val / a), 0);
 
 		return xArr.length / b - xArr.length * mathjs.log(a) + lnSum - arraySum;
 	}
 
-	var f11 = function(a, b, xArr)
+	let f11 = (a, b, xArr) =>
 	{
-		var arraySum = 0;
-
-		for(var i = 0; i < xArr.length; ++i)
-		{
-			arraySum += mathjs.pow(xArr[i] / a, b);
-		}
+		let arraySum = xArr.reduce((pre, val) => pre + mathjs.pow(val / a, b), 0);
 
 		return b * (xArr.length - (b + 1) * arraySum) / a**2;
 	}
 
-	var f12 = function(a, b, xArr)
+	let f12 = (a, b, xArr) =>
 	{
-		var sum1 = 0, arraySum = 0;
-
-		for(var i = 0; i < xArr.length; ++i)
-		{
-			sum1 += mathjs.pow(xArr[i] / a, b);
-
-			arraySum += (mathjs.pow(xArr[i] / a, b) * mathjs.log(xArr[i] / a));
-		}
+		let sum1 = xArr.reduce((pre, val) => pre + mathjs.pow(val / a, b), 0), 
+			arraySum = xArr.reduce((pre, val) => pre + mathjs.pow(val / a, b) * mathjs.log(val / a), 0);
 
 		return -(xArr.length - sum1 - b * arraySum) / a;
 	}
 
-	var f21 = f12;
+	let f21 = f12;
 
-	var f22 = function(a, b, xArr)
+	let f22 = (a, b, xArr) =>
 	{
-		var arraySum = 0;
-
-		for(var i = 0; i < xArr.length; ++i)
-		{
-			arraySum += (mathjs.pow(xArr[i] / a, b) * mathjs.log(xArr[i] / a))**2;
-		}
+		let arraySum = xArr.reduce((pre, val) => pre + (mathjs.pow(val / a, b) * mathjs.log(val / a))**2, 0);
 
 		return -xArr.length / b**2 - arraySum;
 	}
@@ -428,12 +355,12 @@ function weibullParameters(data)
 
 // MODULES //
 
-var gamma = require( 'gamma' );
+const gamma = require( 'gamma' );
 
 
 // VARIABLES //
 
-var EPSILON = 1e-12;
+const EPSILON = 1e-12;
 
 
 // UPPER INCOMPLETE GAMMA FUNCTION
@@ -458,7 +385,7 @@ function gammainc_u( x, s, regularized ) {
 		}
 	}
 
-	var f = 1 + x - s,
+	let f = 1 + x - s,
 		C = f,
 		D = 0,
 		i = 1,
@@ -509,7 +436,7 @@ function gammainc_l( x, s, regularized ) {
 		}
 	}
 
-	var ft,
+	let ft,
 		r = s,
 		c = 1,
 		pws = 1;
@@ -543,19 +470,19 @@ function gammainc_l( x, s, regularized ) {
 
 // MODULES //
 
-var evalrational = require( 'math-evalrational' ).factory;
-var floor = require( 'math-floor' );
-var pow = require( 'math-power' );
-var sinpi = require( 'math-sinpi' );
+const evalrational = require( 'math-evalrational' ).factory;
+const floor = mathjs.floor;
+const pow = mathjs.pow;
+import sinpi from 'math-sinpi';
 
 
 // CONSTANTS //
 
-var Y_OFFSET_2_4 = 3.558437347412109375;
-var PI = require( 'const-pi' );
+const Y_OFFSET_2_4 = 3.558437347412109375;
+const PI = require( 'const-pi' );
 
 // Polynomial coefficients...
-var P_1_2 = [
+const P_1_2 = [
 	-0.999999999999999082554457936871832533,
 	-4.71237311120865266379041700054847734,
 	-7.94125711970499027763789342500817316,
@@ -568,7 +495,8 @@ var P_1_2 = [
 	0.0272103140348194747360175268778415049,
 	0.0015764849020876949848954081173520686
 ];
-var Q_1_2 = [
+
+const Q_1_2 = [
 	1.0,
 	4.71237311120863419878375031457715223,
 	9.58619118655339853449127952145877467,
@@ -581,7 +509,8 @@ var Q_1_2 = [
 	0.00157648490200498142247694709728858139,
 	0.161264050344059471721062360645432809e-14
 ];
-var P_2_4 = [
+
+const P_2_4 = [
 	-2.55843734739907925764326773972215085,
 	-12.2830208240542011967952466273455887,
 	-23.9195022162767993526575786066414403,
@@ -595,7 +524,8 @@ var P_2_4 = [
 	0.002373665205942206348500250056602687,
 	0
 ];
-var Q_2_4 = [
+
+const Q_2_4 = [
 	1.0,
 	4.80098558454419907830670928248659245,
 	9.99220727843170133895059300223445265,
@@ -609,7 +539,8 @@ var Q_2_4 = [
 	0.239554887903526152679337256236302116e-15,
 	-0.294749244740618656265237072002026314e-17
 ];
-var P_4_8 = [
+
+const P_4_8 = [
 	0.166626112697021464248967707021688845e-16,
 	0.499999999999997739552090249208808197,
 	6.40270945019053817915772473771553187,
@@ -622,7 +553,8 @@ var P_4_8 = [
 	487.268001604651932322080970189930074,
 	119.953445242335730062471193124820659
 ];
-var Q_4_8 = [
+
+const Q_4_8 = [
 	1.0,
 	12.4720855670474488978638945855932398,
 	78.6093129753298570701376952709727391,
@@ -635,7 +567,8 @@ var Q_4_8 = [
 	119.952317857277045332558673164517227,
 	0.000140165918355036060868680809129436084
 ];
-var P_8_16 = [
+
+const P_8_16 = [
 	-0.184828315274146610610872315609837439e-19,
 	0.500000000000000004122475157735807738,
 	3.02533865247313349284875558880415875,
@@ -647,7 +580,8 @@ var P_8_16 = [
 	35.8621515614725564575893663483998663,
 	8.72152231639983491987779743154333318
 ];
-var Q_8_16 = [
+
+const Q_8_16 = [
 	1.0,
 	5.71734397161293452310624822415866372,
 	25.293404179620438179337103263274815,
@@ -659,7 +593,8 @@ var Q_8_16 = [
 	8.89898032477904072082994913461386099,
 	-0.0296627336872039988632793863671456398
 ];
-var P_16_INF = [
+
+const P_16_INF = [
 	0.0,
 	0.500000000000000000000000000000087317,
 	0.345625669885456215194494735902663968,
@@ -672,7 +607,8 @@ var P_16_INF = [
 	23.9292359711471667884504840186561598,
 	0
 ];
-var Q_16_INF = [
+
+const Q_16_INF = [
 	1.0,
 	0.357918006437579097055656138920742037,
 	19.1386039850709849435325005484512944,
@@ -688,11 +624,11 @@ var Q_16_INF = [
 
 // Compile functions to evaluate rational functions based on the above coefficients...
 
-var rateval_1_2 = evalrational( P_1_2, Q_1_2 );
-var rateval_2_4 = evalrational( P_2_4, Q_2_4 );
-var rateval_4_8 = evalrational( P_4_8, Q_4_8 );
-var rateval_8_16 = evalrational( P_8_16, Q_8_16 );
-var rateval_16_INF = evalrational( P_16_INF, Q_16_INF );
+const rateval_1_2 = evalrational( P_1_2, Q_1_2 );
+const rateval_2_4 = evalrational( P_2_4, Q_2_4 );
+const rateval_4_8 = evalrational( P_4_8, Q_4_8 );
+const rateval_8_16 = evalrational( P_8_16, Q_8_16 );
+const rateval_16_INF = evalrational( P_16_INF, Q_16_INF );
 
 
 // TRIGAMMA //
@@ -705,10 +641,10 @@ var rateval_16_INF = evalrational( P_16_INF, Q_16_INF );
 * @returns {Number} function value
 */
 function trigamma( x ) {
-	var result = 0;
-	var s;
-	var y;
-	var z;
+	let result = 0;
+	let s;
+	let y;
+	let z;
 	// Check for negative arguments and use reflection:
 	if ( x <= 0 ) {
 		z = 1 - x;
@@ -758,7 +694,7 @@ function trigamma( x ) {
 
 module.exports =
 {
-    "gamma" : gamma,
+    "gamma" : cgamma,
     "erf" : erf,
 	"mean" : mean,
 	"means" : means,
--- StatisticalTest/test/sp_math.test.js
+++ StatisticalTest/test/sp_math.test.js
@@ -1,4 +1,10 @@
-import {erf, gamma, mean, means} from '../mylib/sp_math';
+import {readFileSync} from 'fs';
+
+import {erf, gamma, trigamma, mean, means, gammaParameters, betaParameters, weibullParameters} from '../mylib/sp_math';
+
+const aqiData = JSON.parse(readFileSync('./dataset/aqi_dist.json'));
+
+// const betaSynData = JSON.parse(readFileSync('./dataset/beta_dist1.json'));
 
 /**
  * Tests of erf function.
@@ -24,6 +30,13 @@ test("gamma(1) should be 1.", () => {
 
 test("erf(1000) should be 1.", () => {
     expect(gamma(2.1)).toBeCloseTo(1.046486);
+});
+
+/**
+ * Tests of trigamma().
+ */
+test("trigamma(1) should be 1.644934.", () => {
+    expect(trigamma(1)).toBeCloseTo(1.644934);
 });
 
 /**
@@ -63,3 +76,58 @@ test("means([[1, null], [1, null], [1, 4], [1, null]]) should be [1, 1].", () =>
 test("means([[1, null], [1, null], [1, null], [1, null]]) should be [1, 1].", () => {
     expect(means([[1, null], [1, null], [1, null], [1, null]])).toEqual([1, 0]);
 });
+
+/**
+ * Tests of gammaParameters().
+ */
+test("gammaParameters(aqiData) should be {\"shape1\": 6.0726199636486, \"shape2\": 0.0861069903735485}", () => {
+    // expect(gammaParameters(aqiData)).toEqual({"shape1" : 1, "shape2" : 2});
+    let result = gammaParameters(aqiData);
+    expect(result["shape1"]).toBeCloseTo(6.0726199636486);
+    expect(result["shape2"]).toBeCloseTo(0.0861069903735485);
+});
+
+test("gammaParameters([]) should be {}", () => {
+    expect(gammaParameters([])).toEqual({});
+});
+
+/**
+ * Tests of betaParameters().
+ */
+test("betaParameters(aqiData) should be {}", () => {
+    let result = betaParameters(aqiData);
+    expect(result).toEqual({});
+});
+
+test("betaParameters([]) should be {}", () => {
+    let result = betaParameters([]);
+    expect(result).toEqual({});
+});
+
+// test("betaParameters(aqiData) should be {}", () => {
+//     let result = betaParameters(aqiData);
+//     expect(result).toEqual({});
+// });
+
+/**
+ * Tests of weibullParameters().
+ */
+test("weibullParameters(aqiData) should be {\"shape1\": 79.678440486807, \"shape2\": 2.40870475769043}", () => {
+    let result = weibullParameters(aqiData);
+    expect(result["shape1"]).toBeCloseTo(79.678440486807);
+    expect(result["shape2"]).toBeCloseTo(2.40870475769043);
+});
+
+/**
+ * Other tests.
+ */
+// test("aqiData.reduce(val => Math.log(val), 0) == logSum?", () => {
+//     let logSum = 0;
+
+//     for(let val of aqiData)
+//     {
+//         logSum += Math.log(val);
+//     }
+
+//     expect(aqiData.reduce((pre, val) => {return pre + Math.log(val)})).toBe(logSum);
+// });
