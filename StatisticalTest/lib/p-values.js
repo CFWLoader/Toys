566caa407235d9a88c877108fd5e9777b4ba4b85
CFWLoader <cfwloader@gmail.com>
2018-03-30 16:55:17 +0800

    Only gamma's AD value cannot pass.

--- StatisticalTest/lib/p-values.js
+++ StatisticalTest/lib/p-values.js
@@ -1,1 +1,74 @@
 'use strict';
+
+import {log, abs} from 'mathjs';
+
+/**
+ * Calculate AD-Value and KD-Value on transformed and sorted data.
+ * @param {Array[]} data 
+ * @param {Map} dataShape
+ * @return {Array[]}
+ */
+function measures(data, dataShape)
+{
+    if(!Array.isArray(data) || !Array.isArray(data[0]) || data.length == 0)
+    {
+        return [];
+    }
+
+    // Initializations.
+    let dataLen = dataShape.get('validLength');
+
+    let numOfDist = data.length;
+
+    let adSums = new Array(numOfDist);
+
+    let dplus = new Array(numOfDist), dminus = new Array(numOfDist);
+
+    for(let distIdx = 0; distIdx < numOfDist; ++distIdx)
+    {
+        adSums[distIdx] = 0, dplus[distIdx] = 0, dminus[distIdx] = 0;
+    }
+
+    // Temporary variables.
+    let dp, dm, iVal, n_iVal, mulVal, wi = 1;
+
+    for(let dataIdx = 0; dataIdx < dataLen; ++dataIdx, ++wi)
+    {
+        for(let distIdx = 0; distIdx < numOfDist; ++distIdx)
+        {
+            // Calculate single AD value.
+            iVal = data[distIdx][dataIdx], n_iVal = 1 - data[distIdx][dataLen - 1 - dataIdx];
+
+            mulVal = iVal * n_iVal;
+
+            if(mulVal != 0)
+            {
+                adSums[distIdx] += (2 * dataIdx + 1) * log(mulVal);
+            }
+
+            // Single KS value.
+            dp = abs(iVal - wi / dataLen), dm = abs(iVal - (wi - 1) / dataLen);
+
+            if(dp > dplus[distIdx])
+            {
+                dplus[distIdx] = dp;
+            }
+
+            if(dm > dminus[distIdx])
+            {
+                dminus = dm;
+            }
+        }
+    }
+
+    let result = new Array(numOfDist);
+
+    for(let distIdx = 0; distIdx < numOfDist; ++distIdx)
+    {
+        result[distIdx] = [- dataLen - adSums[distIdx] / dataLen, dplus[distIdx] > dminus[distIdx] ? dplus[distIdx] : dminus[distIdx]];
+    }
+
+    return result;
+}
+
+export{measures};
---
+++ StatisticalTest/test/p-value.test.js
@@ -0,0 +1,110 @@
+'use strict';
+
+import {measures} from '../lib/p-values';
+
+import {aqiData} from './test-datasets';
+
+import {shape} from '../lib/shape';
+
+import {filterDistributions} from '../lib/filter-distributions';
+
+import {prepareCalculations} from '../lib/prepare-calculations';
+
+import {DEFAULT_CONSTRIBUTION_NAME_LIST} from '../lib/constants';
+
+import mathjs from 'mathjs';
+
+const allDistributions = DEFAULT_CONSTRIBUTION_NAME_LIST.slice(0);
+
+allDistributions.push('beta');
+
+allDistributions.push('gamma');
+
+allDistributions.push('weibull');
+
+// test('Test all on AQI dataset.', () => {
+
+//     let data = aqiData;
+
+//     let dataShape = shape(aqiData);                            // Pass 1, T(n).
+
+//     // @TODO
+//     // pseudo:fixNullCell(data, fixStrategy)
+
+//     // Using default distributions to test if null.
+
+//     let availDist = filterDistributions(dataShape, allDistributions);
+
+//     let numOfDist = availDist.length, dataLen = dataShape.get('validLength');
+
+//     let calculations = prepareCalculations(dataShape, availDist, data);
+
+//     let transformedMatrix = availDist.map(() => new Array(dataLen));
+
+//     for(let dataIdx = 0; dataIdx < dataLen; ++dataIdx)      // Pass 2, T(n) = n * numOfDist.
+//     {
+//         for(let calIdx = 0; calIdx < numOfDist; ++calIdx)
+//         {
+//             transformedMatrix[calIdx][dataIdx] = calculations[calIdx](data[dataIdx]);
+//         }
+//     }
+
+//     let sortedTransMat = new Array(numOfDist);
+
+//     for(let calIdx = 0; calIdx < numOfDist; ++calIdx)       // Pass 3, T(nlogn) = n*log(n) * numOfDist. Assume Mathjs uses T(nlogn).
+//     {
+//         sortedTransMat[calIdx] = mathjs.sort(transformedMatrix[calIdx]);
+//     }
+
+//     let result = measures(sortedTransMat, dataShape);
+
+//     // console.log(availDist);
+
+//     expect(result[0][0]).toBeCloseTo(291.2782761317, 10);
+//     expect(result[1][0]).toBeCloseTo(19.7474369825644, 10);
+//     expect(result[2][0]).toBeCloseTo(1.10149026748664, 10);
+//     expect(result[3][0]).toBeCloseTo(61.1310452265662, 10);
+//     expect(result[4][0]).toBeCloseTo(154.31339412696, 10);
+//     expect(result[5][0]).toBeCloseTo(4.30165803301067, 10);
+//     expect(result[6][0]).toBeCloseTo(14.4191153154662, 5);
+// });
+
+test('Test gamma on AQI dataset.', () => {
+
+    let data = aqiData;
+
+    let dataShape = shape(aqiData);                            // Pass 1, T(n).
+
+    // @TODO
+    // pseudo:fixNullCell(data, fixStrategy)
+
+    // Using default distributions to test if null.
+
+    let availDist = filterDistributions(dataShape, ['gamma']);
+
+    let numOfDist = availDist.length, dataLen = dataShape.get('validLength');
+
+    let calculations = prepareCalculations(dataShape, availDist, data);
+
+    let transformedMatrix = availDist.map(() => new Array(dataLen));
+
+    for(let dataIdx = 0; dataIdx < dataLen; ++dataIdx)      // Pass 2, T(n) = n * numOfDist.
+    {
+        for(let calIdx = 0; calIdx < numOfDist; ++calIdx)
+        {
+            transformedMatrix[calIdx][dataIdx] = calculations[calIdx](data[dataIdx]);
+        }
+    }
+
+    let sortedTransMat = new Array(numOfDist);
+
+    for(let calIdx = 0; calIdx < numOfDist; ++calIdx)       // Pass 3, T(nlogn) = n*log(n) * numOfDist. Assume Mathjs uses T(nlogn).
+    {
+        sortedTransMat[calIdx] = mathjs.sort(transformedMatrix[calIdx]);
+    }
+
+    let result = measures(sortedTransMat, dataShape);
+
+    // console.log(availDist);
+    expect(result[0][0]).toBeCloseTo(4.30165803301067, 10);
+});
