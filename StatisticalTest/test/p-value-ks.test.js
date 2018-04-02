8ecdfb7ebcedaf674761fbc86b62d4819e5650e5
CFWLoader <cfwloader@gmail.com>
2018-04-02 17:54:07 +0800

    Off-duty.

--- StatisticalTest/test/p-value-ks.test.js
+++ StatisticalTest/test/p-value-ks.test.js
@@ -1,1 +1,20 @@
 'use strict';
+
+import {KolmogorovSmirnov} from '../lib/p-values';
+
+import * as mathjs from 'mathjs';
+
+/**
+ * Tests of KSE.uniform().
+ */
+
+// pass. May be add test cases in future.
+
+/**
+ * Tests of KSE.normality().
+ */
+test('[D=0.7]KSE.normality(D/(sqrt(233) - 0.01 + 0.85/sqrt(233), Map{\'validLength\'=233}) should be 0.15.', () => {
+    let d = 0.7, params = new Map([['validLength', 233]]);
+
+    expect(KolmogorovSmirnov.normality(D/(sqrt(233) - 0.01 + 0.85/sqrt(233), params))).toBe(0.15);
+});
